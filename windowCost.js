a8378fd6b3e1ef259e7789daf37fd0d269782a61
KJEzorro <gndlg91@gmail.com>
2021-11-11 11:03:28 +0900

    Initial commit

---
+++ windowCost.js
@@ -0,0 +1,22 @@
+
+// 1pt = 304.8mm
+
+// 1pt = 3.5$
+
+// 가로 300mm x 세로 200mm
+
+const pt = 304.8
+const rebC = 3.5
+
+const width = 300
+const ver = 200
+
+const ptt = ((width + ver * 2) / pt)
+
+const totalC = (ptt * rebC)
+
+console.log(totalC)
+
+
+
+
