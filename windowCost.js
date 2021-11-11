9547280e4eff5ca6e17c3d6b50b71952ee5ad926
KJEzorro <gndlg91@gmail.com>
2021-11-11 20:10:05 +0900

    good

--- windowCost.js
+++ windowCost.js
@@ -1,19 +1,19 @@
 
-// 1pt = 304.8mm
+// 1ft = 304.8mm
 
-// 1pt = 3.5$
+// 1ft = 3.5$
 
 // 가로 300mm x 세로 200mm
 
-const pt = 304.8
+const ft = 304.8
 const rebC = 3.5
 
 const width = 300
 const ver = 200
 
-const ptt = ((width + ver * 2) / pt)
+const ftt = ((width + ver * 2) / ft)
 
-const totalC = (ptt * rebC)
+const totalC = (ftt * rebC)
 
 console.log(totalC)
 
